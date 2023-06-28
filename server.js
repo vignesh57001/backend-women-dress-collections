5fa539aeb16e4d50a7f4beeaeb80217f46a8d771
vignesh57001 <vigneshmenon2303@gmail.com>
2023-06-28 20:53:54 +0530

    cors install

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const Dress = require("./modelss/dressModel");
 
